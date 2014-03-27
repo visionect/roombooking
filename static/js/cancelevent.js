84066ba77ce5376074eea26577d4754dc90c0fc3
Matevz Mihalic <matevz.mihalic@gmail.com>
2014-03-27 16:09:24 +0100

    Implemented cancel event.

--- static/js/cancelevent.js
+++ static/js/cancelevent.js
@@ -1,11 +1,11 @@
 function handleClientLoad() {
     $(document).ready(function() {
-        $.getJSON('/user', function(data) {
-            if (! ('error' in data)) {
-                $('#user').html(data.email);
-                gapi.auth.setToken(data.access_token);
+        $.getJSON('/user', function(userdata) {
+            if (! ('error' in userdata)) {
+                $('#user').html(userdata.email);
+                gapi.auth.setToken(userdata.access_token);
                 gapi.auth.authorize({
-                    client_id: data.client_id,
+                    client_id: userdata.client_id,
                     scope: 'https://www.googleapis.com/auth/calendar',
                     immediate: true
                 }, function() {
@@ -26,29 +26,17 @@ function handleClientLoad() {
                             gapi.client.load('calendar', 'v3', function() {
                                 $('button').prop('disabled', false).click(function(e) {
                                     e.preventDefault();
-                                    //Implement this
-                                    /*
-                                    gapi.client.calendar.events.insert({
-                                        calendarId: data.calendarId,
-                                        resource: {
-                                            summary: $('#summary').val(),
-                                            location: $('#location').val(),
-                                            start: {
-                                                dateTime: $('#start').val()
-                                            },
-                                            end: {
-                                                dateTime: $('#end').val()
-                                            }
-                                        }
+                                    gapi.client.calendar.events.delete({
+                                        calendarId: userdata.calendarId,
+                                        eventId: events[0].id
                                     }).execute(function(data) {
                                         if (data && !data.error) {
                                             $('form').remove();
-                                            $('.alert').removeClass('alert-danger hidden').addClass('alert-success').html('<strong>Event added:</strong> Edit it on <a href="' + data.htmlLink + '">Google Calendar</a>');
+                                            $('.alert').removeClass('alert-danger hidden').addClass('alert-success').html('<strong>Event removed</strong>');
                                         } else {
                                             $('.alert').html('<strong>Error:</strong> ' + data.message).removeClass('hidden');
                                         }
                                     });
-                                    */
                                 });
                             });
                         }
