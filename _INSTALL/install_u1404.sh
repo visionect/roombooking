408d0504bd6347ebd18d5072c47a80d2e1da9944
Luka Birsa <luka.birsa@visionect.si>
2014-05-07 18:06:19 +0200

    Fixes for crontab installer.

--- _INSTALL/install_u1404.sh
+++ _INSTALL/install_u1404.sh
@@ -9,33 +9,33 @@ echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     # do dangerous stuff
-    echo "\n**** Download requirements\n"
+    printf "\n**** Download requirements\n"
     sudo apt-get install git python-virtualenv supervisor nginx
     
-    echo "\n**** Cloning the repository\n"
+    printf "\n**** Cloning the repository\n"
     git clone https://github.com/visionect/roombooking.git $INSTALL_DIR
     cd $INSTALL_DIR
     
-    echo "\n**** Create virtual env\n"
+    printf "\n**** Create virtual env\n"
     virtualenv ./virt
     
-    echo "\n**** Activating virtual env and installing packages\n"
+    printf "\n**** Activating virtual env and installing packages\n"
     source ./virt/bin/activate
     pip install -r requirements.txt
 
-    echo "\n**** Creating a new config file\n"
-    echo "**** MAKE SURE YOU EDIT $INSTALL_DIR/server.conf AND ADD YOUR GOOGLE APPS CREDENTIALS!\n"
+    printf  "\n**** Creating a new config file\n"
+    printf "**** MAKE SURE YOU EDIT $INSTALL_DIR/server.conf AND ADD YOUR GOOGLE APPS CREDENTIALS!\n"
     cp $INSTALL_DIR/server.conf.example $INSTALL_DIR/server.conf
 
-    echo "\n**** Installing the supervisord service in the local users crontab\n"
+    printf "\n**** Installing the supervisord service in the local users crontab\n"
     COMMAND="$INSTALL_DIR/start_roombooking.sh"
-    JOB="@reboot $command"
-    cat <(fgrep -i -v "$command" <(crontab -l)) <(echo "$job") | crontab -
+    JOB="@reboot $COMMAND"
+    cat <(fgrep -i -v "$COMMAND" <(crontab -l)) <(echo "$JOB") | crontab -
 
-    echo "\n**** Symlinking the configuration for NGINX, service will be running on port 8000.\n"
+    printf "\n**** Symlinking the configuration for NGINX, service will be running on port 8000.\n"
     sudo ln -sfn $INSTALL_DIR/roombooking-nginx.conf /etc/nginx/sites-enabled/roombooking-nginx.conf
     sudo service nginx restart
 
-    echo "\n**** Done installing! Make sure you edit $INSTALL_DIR/server.conf (ie: nano $INSTALL_DIR/server.conf)\n"
-    echo "**** You'll need to restart the server afterwards with $INSTALL_DIR/restart_roombooking.sh\n"
+    printf "\n**** Done installing! Make sure you edit $INSTALL_DIR/server.conf (ie: nano $INSTALL_DIR/server.conf)\n"
+    printf "**** You'll need to restart the server afterwards with $INSTALL_DIR/restart_roombooking.sh\n"
 fi
